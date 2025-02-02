e5a22e1723187b5c9d028cfe8cee604ab53a33d1
LEEEUNJI <eunjilee3008@gmail.com>
2025-02-02 22:02:07 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/testcontainers_demo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
